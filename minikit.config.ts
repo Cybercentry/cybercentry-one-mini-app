559a2ba8a1efb67043072783fe92225d41b769b9
Cybercentry <info@cybercentry.co.uk>
2025-10-12 13:30:24 +0200

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -12,7 +12,7 @@ export const minikitConfig = {
     header: "eyJmaWQiOjEzMDIzOTIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyODI2RWFlRmMzRmYzNzk0OTE1ODljNUJGNTNmMDI2MTk5ZEVDOUE0In0",
     payload: "eyJkb21haW4iOiJjeWJlcmNlbnRyeS1vbmUtbWluaS1hcHAudmVyY2VsLmFwcCJ9",
     signature: "MHhhMjI3YzU2MTM4NjJmMmQzODgyNzNjZGI0YTc0M2JhNDdhYzVkOTkxYmMyNjRkMjZlZTQ5OThhZGMwZTEyMzE0MzcwYTMzZTA3OGYzY2NiYzUwMDkzZDg5YWZiMzY1NTMzYjM0NWFkNWY3ODU4M2M1MmE3MGYyY2JkNTg0ODk3NTFi"
-  },
+  }
   "baseBuilder": {
     "allowedAddresses": ["0xfee13309251b632317ea2d475d6aba7e7e0219e6"]
   },
