3545f9d83d266fee4182dfa1251b41c2d8fc1276
Cybercentry <info@cybercentry.co.uk>
2025-10-14 05:13:00 +0200

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -8,32 +8,36 @@ const ROOT_URL =
  * @see {@link https://miniapps.farcaster.xyz/docs/guides/publishing}
  */
 export const minikitConfig = {
-    "accountAssociation": {
-    header: "eyJmaWQiOjEzMDIzOTIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyODI2RWFlRmMzRmYzNzk0OTE1ODljNUJGNTNmMDI2MTk5ZEVDOUE0In0",
-    payload: "eyJkb21haW4iOiJjeWJlcmNlbnRyeS1vbmUtbWluaS1hcHAudmVyY2VsLmFwcCJ9",
-    signature: "MHhhMjI3YzU2MTM4NjJmMmQzODgyNzNjZGI0YTc0M2JhNDdhYzVkOTkxYmMyNjRkMjZlZTQ5OThhZGMwZTEyMzE0MzcwYTMzZTA3OGYzY2NiYzUwMDkzZDg5YWZiMzY1NTMzYjM0NWFkNWY3ODU4M2M1MmE3MGYyY2JkNTg0ODk3NTFi"
-  },
-  "baseBuilder": {
-    "allowedAddresses": ["0xfee13309251b632317ea2d475d6aba7e7e0219e6"]
+  "accountAssociation": {
+    "header": "eyJmaWQiOjEzMDIzOTIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyODI2RWFlRmMzRmYzNzk0OTE1ODljNUJGNTNmMDI2MTk5ZEVDOUE0In0",
+    "payload": "eyJkb21haW4iOiJjeWJlcmNlbnRyeS1vbmUtbWluaS1hcHAudmVyY2VsLmFwcCJ9",
+    "signature": "MHhhMjI3YzU2MTM4NjJmMmQzODgyNzNjZGI0YTc0M2JhNDdhYzVkOTkxYmMyNjRkMjZlZTQ5OThhZGMwZTEyMzE0MzcwYTMzZTA3OGYzY2NiYzUwMDkzZDg5YWZiMzY1NTMzYjM0NWFkNWY3ODU4M2M1MmE3MGYyY2JkNTg0ODk3NTFi"
   },
   "miniapp": {
     "version": "1",
-    name: "Cybercentry One", 
-    subtitle: "Cyber Threat Response", 
-    description: "Empowers individuals and organisations to anticipate, prevent, and respond to cyber threats with confidence.",
-    screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
-    iconUrl: `${ROOT_URL}/blue-icon.png`,
-    splashImageUrl: `${ROOT_URL}/blue-hero.png`,
-    splashBackgroundColor: "#ffffff",
-    homeUrl: ROOT_URL,
-    webhookUrl: `${ROOT_URL}/api/webhook`,
-    primaryCategory: "utility",
-    tags: ["cybersecurity", "subscription", "msp", "mdr", "compliance"],
-    heroImageUrl: `${ROOT_URL}/blue-hero.png`, 
-    tagline: "",
-    ogTitle: "",
-    ogDescription: "",
-    ogImageUrl: `${ROOT_URL}/blue-hero.png`,
+    "name": "Cybercentry One",
+    "subtitle": "Cyber Threat Response",
+    "description": "Empowers individuals and organisations to anticipate, prevent, and respond to cyber threats with confidence.",
+    "screenshotUrls": [
+      "https://cybercentry-one-mini-app.vercel.app/screenshot-portrait.png"
+    ],
+    "iconUrl": "https://cybercentry-one-mini-app.vercel.app/blue-icon.png",
+    "splashImageUrl": "https://cybercentry-one-mini-app.vercel.app/blue-hero.png",
+    "splashBackgroundColor": "#ffffff",
+    "homeUrl": "https://cybercentry-one-mini-app.vercel.app",
+    "webhookUrl": "https://cybercentry-one-mini-app.vercel.app/api/webhook",
+    "primaryCategory": "utility",
+    "tags": [
+      "cybersecurity",
+      "subscription",
+      "msp",
+      "mdr",
+      "compliance"
+    ],
+    "heroImageUrl": "https://cybercentry-one-mini-app.vercel.app/blue-hero.png",
+    "ogImageUrl": "https://cybercentry-one-mini-app.vercel.app/blue-hero.png"
   },
-} as const;
-
+  "baseBuilder": {
+    "allowedAddresses": ["0xfee13309251b632317ea2d475d6aba7e7e0219e6"]
+  }
+}
