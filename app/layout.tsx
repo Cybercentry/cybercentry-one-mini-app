77e4fd18cbf61fdaf0dbaeea3592861f49ac14c1
Cybercentry <info@cybercentry.co.uk>
2025-10-17 21:39:05 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,22 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Karla } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
+import { Karla, Source_Code_Pro } from "next/font/google"
 import "./globals.css"
 
-const karla = Karla({ subsets: ["latin"] })
-
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Cybercentry One",
+  description: "Smarter, simpler, and more accessible cyber security awaits.",
 }
+
+const karla = Karla({
+  variable: "--font-karla",
+  subsets: ["latin"],
+})
+
+const sourceCodePro = Source_Code_Pro({
+  variable: "--font-source-code-pro",
+  subsets: ["latin"],
+})
 
 export default function RootLayout({
   children,
@@ -19,10 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans antialiased`}>
-        {children}
-        <Analytics />
-      </body>
+      <body className={`${karla.variable} ${sourceCodePro.variable}`}>{children}</body>
     </html>
   )
 }
+
